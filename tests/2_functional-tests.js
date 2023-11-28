104cb4d3c07d73d8366a00ad908ccc458617a77f
Madina <madinatussupova0820@gmail.com>
2023-11-28 13:29:37 +0600

    fix browser.fill()

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -109,29 +109,25 @@ suite('Functional Tests with Zombie.js', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
       browser.fill('surname', 'Colombo')
-        .then(() => {
-          browser.pressButton('submit', () => {
+          .pressButton('submit', () => {
             browser.assert.success()
             browser.assert.text('span#name', 'Cristoforo')
             browser.assert.text('span#surname', 'Colombo')
             browser.assert.element('span#dates', 1)
             done()
           })
-        })
       done();
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
       browser.fill('surname', 'Vespucci')
-        .then(() => {
-          browser.pressButton('submit', () => {
+          .pressButton('submit', () => {
             browser.assert.success()
             browser.assert.text('span#name', 'Amerigo')
             browser.assert.text('span#surname', 'Vespucci')
             browser.assert.element('span#dates', 1)
             done()
           })
-        })
       done();
     });
   });
